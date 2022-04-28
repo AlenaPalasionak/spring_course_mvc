fce670bdb54626b0f32bb8d113ef2276ba731325
Lena Palasionak <19lenka87@tut.by>
2022-04-28 10:38:53 +0300

    SpringMVC.4 @RequestParam

--- src/main/java/com/lenapalasionak/spring/mvc/MyController.java
+++ src/main/java/com/lenapalasionak/spring/mvc/MyController.java
@@ -3,6 +3,7 @@ package com.lenapalasionak.spring.mvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -24,10 +25,18 @@ public class MyController {
 //    public String showEmpDedails() {
 //        return "show-emp-details-view";
 //    }
+//    @RequestMapping("/showDetails")
+//    public String showEmpDedails(HttpServletRequest request, Model model) {
+//
+//        String empName = request.getParameter("employeeName");
+//        empName = "Mrs " + empName;
+//        model.addAttribute("nameAttribute", empName);
+//
+//        return "show-emp-details-view";
+//    }
     @RequestMapping("/showDetails")
-    public String showEmpDedails(HttpServletRequest request, Model model) {
+    public String showEmpDedails(@RequestParam("employeeName") String empName, Model model) {
 
-        String empName = request.getParameter("employeeName");
         empName = "Mrs " + empName;
         model.addAttribute("nameAttribute", empName);
 
