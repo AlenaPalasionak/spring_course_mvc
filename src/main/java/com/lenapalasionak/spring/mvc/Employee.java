0f0039070c2b9c608c5395a87b00a450f6f26e0b
Lena Palasionak <19lenka87@tut.by>
2022-04-28 16:08:03 +0300

    SpringMVC. 7. Добавлен класс Employee.

---
+++ src/main/java/com/lenapalasionak/spring/mvc/Employee.java
@@ -0,0 +1,53 @@
+package com.lenapalasionak.spring.mvc;
+
+public class Employee {
+    private String name;
+    private String surname;
+    private int salary;
+    private String Department;
+
+    public Employee() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public String getDepartment() {
+        return Department;
+    }
+
+    public void setDepartment(String department) {
+        Department = department;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", salary=" + salary +
+                ", Department='" + Department + '\'' +
+                '}';
+    }
+}
